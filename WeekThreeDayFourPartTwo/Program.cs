0e62fb06b0f786e47ce3d881d8169a9357688bd4
aalhaif <Abdullah.Alhaif@Tuwaiq.edu.sa>
2021-04-28 16:46:31 +0300

    File has been edited

--- WeekThreeDayFourPartTwo/Program.cs
+++ WeekThreeDayFourPartTwo/Program.cs
@@ -6,7 +6,7 @@ namespace WeekThreeDayFourPartTwo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello People!");
         }
     }
 }
